d3d56fae293019e0b16bd3272fbd639fc724237d
erg5995 <erg5995@rit.edu>
2021-05-23 15:34:04 -0400

    Made the particle tester work better and added the particle texture, which I forgot last time :p

--- src/main/java/erg/voidcraft/common/item/ItemParticleTester.java
+++ src/main/java/erg/voidcraft/common/item/ItemParticleTester.java
@@ -3,10 +3,14 @@ package erg.voidcraft.common.item;
 import erg.voidcraft.common.init.VoidcraftParticles;
 import erg.voidcraft.common.particle.MiasmaParticleData;
 import net.minecraft.client.world.ClientWorld;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemUseContext;
+import net.minecraft.util.ActionResult;
 import net.minecraft.util.ActionResultType;
+import net.minecraft.util.Hand;
 import net.minecraft.util.math.vector.Vector3d;
 import net.minecraft.world.World;
 
@@ -23,17 +27,13 @@ public class ItemParticleTester extends Item {
     }
 
     @Override
-    public ActionResultType onItemUse(ItemUseContext ctx) {
+    public ActionResult<ItemStack> onItemRightClick(World world, PlayerEntity player, Hand hand) {
 
-        World world = ctx.getWorld();
-
-        if(world.isRemote) {
-
-            PlayerEntity player = ctx.getPlayer();
+        if (world.isRemote) {
 
             final float PARTIAL_TICKS = 1.0f;
             Vector3d eyePos = player.getEyePosition(PARTIAL_TICKS);
-            Vector3d look = player.getLookVec().normalize();
+            Vector3d look = player.getLookVec();
 
             double xpos = eyePos.x;
             double ypos = eyePos.y;
@@ -45,9 +45,9 @@ public class ItemParticleTester extends Item {
 
             final double POSITION_WOBBLE = 0.01;
 
-            for(int i = 0; i < 100; i++) {
+            for (int i = 0; i < 1000; i++) {
 
-                Color tint = new Color(0f, 0f, 0f);
+                Color tint = new Color(0.5f, 0.5f, 0.5f);
                 final double MIN_DIAMETER = 0.05;
                 final double MAX_DIAMETER = 0.40;
                 double diameter = MIN_DIAMETER + (MAX_DIAMETER - MIN_DIAMETER) * random.nextDouble();
@@ -56,13 +56,20 @@ public class ItemParticleTester extends Item {
 
                 zpos += POSITION_WOBBLE * (rand.nextDouble() - 0.5);
                 xpos += POSITION_WOBBLE * (rand.nextDouble() - 0.5);
+
+                xvel += rand.nextDouble() - 0.5;
+                yvel += rand.nextDouble() - 0.5;
+                zvel += rand.nextDouble() - 0.5;
+
+                xvel /= 10;
+                yvel /= 10;
+                zvel /= 10;
+
                 world.addParticle(miasma, false,
                         xpos, ypos, zpos, xvel, yvel, zvel);
             }
-
-            return ActionResultType.SUCCESS;
         }
-        return ActionResultType.PASS;
+        return super.onItemRightClick(world, player, hand);
     }
 
 }
