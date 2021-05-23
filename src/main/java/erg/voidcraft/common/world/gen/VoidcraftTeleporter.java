e422a2caadf4936a8199a62c59e967d247bdade7
erg5995 <erg5995@rit.edu>
2021-05-23 00:29:43 -0400

    merge

--- src/main/java/erg/voidcraft/common/world/gen/VoidcraftTeleporter.java
+++ src/main/java/erg/voidcraft/common/world/gen/VoidcraftTeleporter.java
@@ -1,4 +1,38 @@
 package erg.voidcraft.common.world.gen;
 
-public class VoidcraftTeleporter {
+import net.minecraft.block.PortalInfo;
+import net.minecraft.entity.Entity;
+import net.minecraft.util.math.vector.Vector3d;
+import net.minecraft.world.World;
+import net.minecraft.world.server.ServerWorld;
+import net.minecraftforge.common.util.ITeleporter;
+
+import java.util.function.Function;
+
+public class VoidcraftTeleporter implements ITeleporter {
+
+    @Override
+    public Entity placeEntity(Entity entity, ServerWorld currentWorld, ServerWorld destWorld, float yaw, Function<Boolean, Entity> repositionEntity) {
+        Entity newEntity = entity.getType().create(destWorld);
+        PortalInfo portalInfo = this.getPortalInfo(entity, destWorld, (ServerWorld) -> null);
+        if(portalInfo == null) {
+            return null;
+        }
+        if (newEntity != null) {
+            newEntity.copyDataFromOld(entity);
+            newEntity.setLocationAndAngles(portalInfo.pos.x, portalInfo.pos.y, portalInfo.pos.z, portalInfo.rotationYaw, newEntity.rotationPitch);
+            newEntity.setMotion(portalInfo.motion);
+            destWorld.addFromAnotherDimension(newEntity);
+        }
+        return newEntity;
+    }
+
+    @Override
+    public PortalInfo getPortalInfo(Entity entity, ServerWorld destWorld, Function<ServerWorld, PortalInfo> defaultPortalInfo) {
+        return new PortalInfo(entity.getPositionVec(), Vector3d.ZERO, entity.rotationYaw, entity.rotationPitch);
+    }
+
+    @Override
+    public boolean isVanilla() {return false;}
+
 }
