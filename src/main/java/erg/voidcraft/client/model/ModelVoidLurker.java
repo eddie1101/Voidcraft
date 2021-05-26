53670e78efa04d349c6bb78dec898651bfeeac08
eddie1101 <erg1101@outlook.com>
2021-05-26 16:39:53 -0400

    Using default biped for now. Will revisit models later.

--- src/main/java/erg/voidcraft/client/model/ModelVoidLurker.java
+++ src/main/java/erg/voidcraft/client/model/ModelVoidLurker.java
@@ -12,50 +12,50 @@ import net.minecraft.client.renderer.model.ModelRenderer;
 
 public class ModelVoidLurker extends BipedModel<EntityVoidLurker> {
 
-    private final ModelRenderer Arm1_r1;
-    private final ModelRenderer Arm2_r1;
+//    private final ModelRenderer Arm1_r1;
+//    private final ModelRenderer Arm2_r1;
 
     public ModelVoidLurker() {
 
-        super(1f, 0f, 128, 64);
+        super(1f, 24f, 128, 64);
 
-        head = new ModelRenderer(this);
-        head.setPos(0.0F, -11.0F, 0.0F);
-        head.texOffs(72, 0).addBox(-5.0F, -5.0F, -5.0F, 10.0F, 10.0F, 10.0F, 0.0F, false);
-        head.texOffs(72, 20).addBox(-3.0F, -3.0F, -6.0F, 2.0F, 2.0F, 1.0F, 0.0F, false);
-        head.texOffs(72, 20).addBox(1.0F, -3.0F, -6.0F, 2.0F, 2.0F, 1.0F, 0.0F, false);
-
-        rightArm = new ModelRenderer(this);
-        rightArm.setPos(-7.5F, -6.0F, -0.5F);
-
-
-        Arm1_r1 = new ModelRenderer(this);
-        Arm1_r1.setPos(2.5F, 0.0F, 0.0F);
-        rightArm.addChild(Arm1_r1);
-        setRotationAngle(Arm1_r1, -1.5708F, 0.0F, 0.0F);
-        Arm1_r1.texOffs(52, 0).addBox(-5.0F, 0.0F, -2.5F, 5.0F, 15.0F, 5.0F, 0.0F, false);
-
-        leftArm = new ModelRenderer(this);
-        leftArm.setPos(7.5F, -6.0F, -0.5F);
-
-
-        Arm2_r1 = new ModelRenderer(this);
-        Arm2_r1.setPos(-2.5F, 0.0F, 0.0F);
-        leftArm.addChild(Arm2_r1);
-        setRotationAngle(Arm2_r1, -1.5708F, 0.0F, 0.0F);
-        Arm2_r1.texOffs(52, 20).addBox(0.0F, 0.0F, -2.5F, 5.0F, 15.0F, 5.0F, 0.0F, false);
-
-        rightLeg = new ModelRenderer(this);
-        rightLeg.setPos(-2.5F, 9.0F, -0.5F);
-        rightLeg.texOffs(32, 0).addBox(-2.5F, 0.0F, -2.5F, 5.0F, 15.0F, 5.0F, 0.0F, false);
-
-        leftLeg = new ModelRenderer(this);
-        leftLeg.setPos(2.5F, 9.0F, -0.5F);
-        leftLeg.texOffs(32, 20).addBox(-2.5F, 0.0F, -2.5F, 5.0F, 15.0F, 5.0F, 0.0F, false);
-
-        body = new ModelRenderer(this);
-        body.setPos(0.0F, 1.5F, -0.5F);
-        body.texOffs(0, 0).addBox(-5.0F, -7.5F, -2.5F, 10.0F, 15.0F, 5.0F, 0.0F, false);
+//        head = new ModelRenderer(this);
+//        head.setPos(0.0F, -11.0F, 0.0F);
+//        head.texOffs(72, 0).addBox(-5.0F, -5.0F, -5.0F, 10.0F, 10.0F, 10.0F, 0.0F, false);
+//        head.texOffs(72, 20).addBox(-3.0F, -3.0F, -6.0F, 2.0F, 2.0F, 1.0F, 0.0F, false);
+//        head.texOffs(72, 20).addBox(1.0F, -3.0F, -6.0F, 2.0F, 2.0F, 1.0F, 0.0F, false);
+//
+//        rightArm = new ModelRenderer(this);
+//        rightArm.setPos(-7.5F, -6.0F, -0.5F);
+//
+//
+//        Arm1_r1 = new ModelRenderer(this);
+//        Arm1_r1.setPos(2.5F, 0.0F, 0.0F);
+//        rightArm.addChild(Arm1_r1);
+//        setRotationAngle(Arm1_r1, -1.5708F, 0.0F, 0.0F);
+//        Arm1_r1.texOffs(52, 0).addBox(-5.0F, 0.0F, -2.5F, 5.0F, 15.0F, 5.0F, 0.0F, false);
+//
+//        leftArm = new ModelRenderer(this);
+//        leftArm.setPos(7.5F, -6.0F, -0.5F);
+//
+//
+//        Arm2_r1 = new ModelRenderer(this);
+//        Arm2_r1.setPos(-2.5F, 0.0F, 0.0F);
+//        leftArm.addChild(Arm2_r1);
+//        setRotationAngle(Arm2_r1, -1.5708F, 0.0F, 0.0F);
+//        Arm2_r1.texOffs(52, 20).addBox(0.0F, 0.0F, -2.5F, 5.0F, 15.0F, 5.0F, 0.0F, false);
+//
+//        rightLeg = new ModelRenderer(this);
+//        rightLeg.setPos(-2.5F, 9.0F, -0.5F);
+//        rightLeg.texOffs(32, 0).addBox(-2.5F, 0.0F, -2.5F, 5.0F, 15.0F, 5.0F, 0.0F, false);
+//
+//        leftLeg = new ModelRenderer(this);
+//        leftLeg.setPos(2.5F, 9.0F, -0.5F);
+//        leftLeg.texOffs(32, 20).addBox(-2.5F, 0.0F, -2.5F, 5.0F, 15.0F, 5.0F, 0.0F, false);
+//
+//        body = new ModelRenderer(this);
+//        body.setPos(0.0F, 1.5F, -0.5F);
+//        body.texOffs(0, 0).addBox(-5.0F, -7.5F, -2.5F, 10.0F, 15.0F, 5.0F, 0.0F, false);
     }
 
     @Override
